f6e8363e853b5a5f831fb0d4333066ae7244e5da
rlaehddn0301 <128763302+rlaehddn0301@users.noreply.github.com>
2023-09-12 00:42:33 +0900

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,10 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sorry~~");
+		System.out.println("재밌어서 미안합니다~~");
+	}
+
+}
