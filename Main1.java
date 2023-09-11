83cb006a42ea007896edc17dbe861868a190561a
rlaehddn0301 <128763302+rlaehddn0301@users.noreply.github.com>
2023-09-12 00:57:50 +0900

    Add files via upload

--- Main1.java
+++ Main1.java
@@ -3,8 +3,10 @@ public class Main1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("sorry~~");
+		System.out.println("Sorry~~");
 		System.out.println("재밌어서 미안합니다~~");
 	}
 
 }
+
+
--- Main2.java
+++ Main2.java
@@ -3,7 +3,8 @@ public class Main2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Wellcom!!");
+		System.out.println("자바 세계로 오신걸 환영합니다.");
 	}
 
 }
